9ee177d9b5579a008e1a086204e74028d530c38b
Yon48 <151472091+Yon48@users.noreply.github.com>
2024-01-26 01:15:55 +0300

    VR1

---
+++ VR-TOUR/data.js
@@ -0,0 +1,755 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-and-dining-view-1",
+      "name": "Living and Dining view 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.48552699620284656,
+          "pitch": 0.3401586736661333,
+          "rotation": 0,
+          "target": "1-living-and-dining-view-2"
+        },
+        {
+          "yaw": 0.6648248980467812,
+          "pitch": 0.13204836364805672,
+          "rotation": 0,
+          "target": "2-living-and-dining-view-3"
+        },
+        {
+          "yaw": 0.2015202998953729,
+          "pitch": -0.006526816917951095,
+          "rotation": 0,
+          "target": "3-open-kitchen-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-and-dining-view-2",
+      "name": "Living and Dining view 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18655454582108533,
+          "pitch": 0.22107724773101012,
+          "rotation": 0,
+          "target": "2-living-and-dining-view-3"
+        },
+        {
+          "yaw": -0.26249071124626155,
+          "pitch": 0.034298768400985225,
+          "rotation": 0,
+          "target": "3-open-kitchen-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-living-and-dining-view-3",
+      "name": "Living and Dining view 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0192054016376098,
+          "pitch": 0.04184402570128398,
+          "rotation": 0,
+          "target": "3-open-kitchen-view-1"
+        },
+        {
+          "yaw": -1.4676106293080124,
+          "pitch": -0.01866739780951754,
+          "rotation": 0,
+          "target": "5-tkitchen-view-1"
+        },
+        {
+          "yaw": -1.5832697592790623,
+          "pitch": -0.017155769503881757,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-open-kitchen-view-1",
+      "name": "Open kitchen view 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6034634996859154,
+          "pitch": -0.0873611415348563,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        },
+        {
+          "yaw": -0.4074846287072216,
+          "pitch": -0.016207260191519524,
+          "rotation": 0,
+          "target": "5-tkitchen-view-1"
+        },
+        {
+          "yaw": -0.5976989537668178,
+          "pitch": 0.04178288486829551,
+          "rotation": 0,
+          "target": "7-children-bed-a-view-1"
+        },
+        {
+          "yaw": 0.045247141444839656,
+          "pitch": 0.17142223193527073,
+          "rotation": 0,
+          "target": "4-open-kitchen-view-2"
+        },
+        {
+          "yaw": -2.094729436469791,
+          "pitch": 0.1971209825856839,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-open-kitchen-view-2",
+      "name": "Open kitchen view 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.383412090783324,
+          "pitch": 0.19378386700368644,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        },
+        {
+          "yaw": -0.8097820858930511,
+          "pitch": 0.040803272857008466,
+          "rotation": 0,
+          "target": "5-tkitchen-view-1"
+        },
+        {
+          "yaw": -0.991999419546584,
+          "pitch": -0.09913234220893408,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        },
+        {
+          "yaw": -0.9962491100937605,
+          "pitch": 0.01915526254721911,
+          "rotation": 0,
+          "target": "7-children-bed-a-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-tkitchen-view-1",
+      "name": "T.kitchen view 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.172800055243865,
+          "pitch": 0.356195327364242,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        },
+        {
+          "yaw": 0.47571390758599286,
+          "pitch": 0.30413104654993894,
+          "rotation": 0,
+          "target": "5-tkitchen-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-tkitchen-view-2",
+      "name": "T.kitchen view 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.881598081464137,
+          "pitch": 0.2358419095923736,
+          "rotation": 0,
+          "target": "5-tkitchen-view-1"
+        },
+        {
+          "yaw": -1.8730104298063903,
+          "pitch": 0.05898244528526675,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-children-bed-a-view-1",
+      "name": "Children bed A view 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8281796352748856,
+          "pitch": -0.1776532283356964,
+          "rotation": 0,
+          "target": "10-children-bed-b-view-1"
+        },
+        {
+          "yaw": -0.8266431880738168,
+          "pitch": -0.052075503999887474,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        },
+        {
+          "yaw": -0.8287663999097674,
+          "pitch": 0.08661426920911985,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-children-bed-a-view-2",
+      "name": "Children bed A view 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3394096199581007,
+          "pitch": 0.07940040612892929,
+          "rotation": 0,
+          "target": "9-children-bed-a-view-3"
+        },
+        {
+          "yaw": 0.5804363298808468,
+          "pitch": 0.07684520980295062,
+          "rotation": 0,
+          "target": "7-children-bed-a-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-children-bed-a-view-3",
+      "name": "Children bed A view 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3944238227623629,
+          "pitch": -0.17201121125080476,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        },
+        {
+          "yaw": -0.39898564352060184,
+          "pitch": -0.039957459247681015,
+          "rotation": 0,
+          "target": "10-children-bed-b-view-1"
+        },
+        {
+          "yaw": -0.3946774942102209,
+          "pitch": 0.10139541665461849,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-children-bed-b-view-1",
+      "name": "Children bed B view 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.916403610177273,
+          "pitch": 0.11166235516378897,
+          "rotation": 0,
+          "target": "11-children-bed-b-view-2"
+        },
+        {
+          "yaw": 0.019219709015597886,
+          "pitch": 0.17559186011608396,
+          "rotation": 0,
+          "target": "2-living-and-dining-view-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-children-bed-b-view-2",
+      "name": "Children bed B view 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.172259186217783,
+          "pitch": -0.23097509845101705,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        },
+        {
+          "yaw": 1.181368344429071,
+          "pitch": -0.1041165690256527,
+          "rotation": 0,
+          "target": "7-children-bed-a-view-1"
+        },
+        {
+          "yaw": 1.1751316502307496,
+          "pitch": 0.025716288941731946,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-children-bed-b-view-3",
+      "name": "Children bed B view 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19297936012683437,
+          "pitch": -0.1492572487876167,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        },
+        {
+          "yaw": -0.18655445948978056,
+          "pitch": -0.021817031134897746,
+          "rotation": 0,
+          "target": "7-children-bed-a-view-1"
+        },
+        {
+          "yaw": -0.18655445948978056,
+          "pitch": 0.11728977671440077,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-master-bedroom",
+      "name": "Master bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8290678565107079,
+          "pitch": -0.12635958606241005,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        },
+        {
+          "yaw": -0.8238291781623097,
+          "pitch": -0.014992828274937864,
+          "rotation": 0,
+          "target": "7-children-bed-a-view-1"
+        },
+        {
+          "yaw": -0.8330023802934434,
+          "pitch": 0.1719128974071431,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        },
+        {
+          "yaw": -0.44847488096391785,
+          "pitch": 0.01940749162122657,
+          "rotation": 0,
+          "target": "14-walkin-closet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-walkin-closet",
+      "name": "Walkin closet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7288989982879368,
+          "pitch": 0.06957946566201301,
+          "rotation": 0,
+          "target": "13-master-bedroom"
+        },
+        {
+          "yaw": -1.0088090140789703,
+          "pitch": -0.1917229472144939,
+          "rotation": 0,
+          "target": "0-living-and-dining-view-1"
+        },
+        {
+          "yaw": -1.0296574854496754,
+          "pitch": -0.04225189587535283,
+          "rotation": 0,
+          "target": "7-children-bed-a-view-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Yared Interior project",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
